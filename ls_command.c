05128f02d63b685b00d0a3db5cdfae3381e3ce4e
thisismichele <michelemessina948@gmail.com>
2025-04-05 17:21:21 +0200

    Improved the ls -l command with new information.

--- ls_command.c
+++ ls_command.c
@@ -1,12 +1,24 @@
 #include "shell.h"
+//metterle globali? uso semafori?
 
 int pistacchio_ls(DIR* path){
     struct dirent* ls;
+    struct stat st;
+    char paths[1024];
+    printf("Permission=P File Name =FN, NL= Number of Link ,UI = User Id ,Last Change= LG\n");
+    printf(LINE);
     while((ls=readdir(path))!=NULL){
         if(ls->d_name[0]!='.'){
-         printf("%hu %s %u  \n",ls->d_reclen,ls->d_name,ls->d_type);
+            
+            snprintf(paths,sizeof(paths),"%s/%s",".",ls->d_name);
+            if(stat(paths,&st)==-1){
+             perror("Error with stat");
+             exit(1);
+            }
+            printf("P:%s FN:%-10s NL:%-10ld UI=%-10d LG:%-30s \r",get_permission(st.st_mode),ls->d_name,(long)st.st_nlink,st.st_uid,ctime(&st.st_ctime));
         }
     }
+    printf(LINE);
     return 0;
 }
 
